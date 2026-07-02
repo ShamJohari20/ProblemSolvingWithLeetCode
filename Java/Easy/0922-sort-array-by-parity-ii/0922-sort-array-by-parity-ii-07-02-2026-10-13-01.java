23cbccd89d2dddedcfe4ff64fef763fce82870fe
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2026-07-02 10:13:01 +0530

    3 ms (58.19%) 48.2 MB (95.45%) 07-02-2026 0922-sort-array-by-parity-ii Easy .java Array

---
+++ Java/Easy/0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii-07-02-2026-10-13-01.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int length = nums.length;
+
+        int even = 0;
+        int odd = 1;
+
+        while(even<length && odd < length){
+            if(nums[even] %2 == 0){
+                even+=2;
+            }else if(nums[odd] %2 == 1){
+                odd +=2;
+            }else{
+                int temp = nums[even];
+                nums[even]= nums[odd];
+                nums[odd]=temp;
+            }
+        }
+        
+
+        return nums;
+        
+    }
+}
