8e0d874ebdf01d1c2e83d589968b4f00a8627497
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2026-07-09 10:29:21 +0530

    1 ms (100%) 47.3 MB (53.55%) 07-09-2026 0977-squares-of-a-sorted-array Easy .java Array

---
+++ Java/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array-07-09-2026-10-29-20.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int length = nums.length;
+
+        int result[] = new int[length];
+
+        int left = 0;
+        int right = length-1;
+        int index = length-1;
+
+        while(left<=right){
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            
+            if(leftSquare > rightSquare){
+                result[index]=leftSquare;
+                left++;
+            }else{
+                result[index]=rightSquare;
+                right--;
+
+            }
+
+            index--;
+            
+        }
+
+        return result;
+    }
+}
