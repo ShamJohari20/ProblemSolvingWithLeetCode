dc1dc7b9895fa430548cac8429d547b129233814
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2026-07-12 23:02:04 +0530

    3 ms (72.81%) 53.4 MB (82.8%) 07-12-2026 0997-find-the-town-judge Easy .java Array

---
+++ Java/Easy/0997-find-the-town-judge/0997-find-the-town-judge-07-12-2026-23-02-04.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+
+        int[] incoming = new int[n + 1];
+        int[] outgoing = new int[n + 1];
+
+        for (int[] t : trust) {
+            outgoing[t[0]]++;
+            incoming[t[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (incoming[i] == n - 1 && outgoing[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
