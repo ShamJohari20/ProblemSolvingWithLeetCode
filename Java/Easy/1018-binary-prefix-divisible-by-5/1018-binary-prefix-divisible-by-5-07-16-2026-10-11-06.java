862e8d8552e686eab74e0bb79f06cf91eeb7bb54
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2026-07-16 10:11:07 +0530

    3 ms (99.09%) 47.4 MB (81.97%) 07-16-2026 1018-binary-prefix-divisible-by-5 Easy .java Array

---
+++ Java/Easy/1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5-07-16-2026-10-11-06.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+
+        List<Boolean> list = new ArrayList<>();
+
+        int rem = 0;
+
+        for(int i : nums){
+            rem = (rem * 2 + i ) % 5;
+            list.add(rem==0);
+        }
+
+        return list;
+    }
+}
