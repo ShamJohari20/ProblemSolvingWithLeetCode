c6a2f1ec31eef022a503cb52cef1567325507043
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2026-07-21 11:08:26 +0530

    29 ms (43.43%) 47 MB (58.08%) 07-21-2026 0387-first-unique-character-in-a-string Easy .java Hash Table

---
+++ Java/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string-07-21-2026-11-08-25.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstUniqChar(String s) {
+
+        Map<Character,Integer> map = new LinkedHashMap<>();
+
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1){
+                return s.indexOf(entry.getKey());
+            }
+        }
+        return -1;
+    }
+}
