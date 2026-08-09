c4c99b76d13b4714a792ee8eca4da174460ab761
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2026-08-09 22:04:23 +0530

    26 ms (99.31%) 76.7 MB (43.45%) 08-09-2026 1331-rank-transform-of-an-array Easy .java Array

---
+++ Java/Easy/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array-08-09-2026-22-04-24.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int[] arrayRankTransform(int[] arr) {
+
+        int[] sorted = arr.clone();
+
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> rank = new HashMap<>();
+
+        int currentRank = 1;
+
+        for (int num : sorted) {
+
+            if (!rank.containsKey(num)) {
+                rank.put(num, currentRank);
+                currentRank++;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rank.get(arr[i]);
+        }
+
+        return arr;
+    }
+}
